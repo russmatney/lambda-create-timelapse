e033347bc7008d957246915e173000d3da32538f
Russell Matney <russell.matney@gmail.com>
2015-03-31 11:24:19 -0400

    deps, gulpfile, test running

---
+++ test/full.spec.js
@@ -0,0 +1,8 @@
+var handler = require('../');
+var expect = require('chai').expect;
+
+describe('timelapse handler', function() {
+  it('should exist', function() {
+    expect(handler).to.exist;
+  });
+});

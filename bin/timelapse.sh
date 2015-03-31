7d8005340f588b074c02642ceab8a1a3dca626b6
Russell Matney <russell.matney@gmail.com>
2015-03-31 15:39:06 -0400

    script draft

--- bin/timelapse.sh
+++ bin/timelapse.sh
@@ -1,8 +1,3 @@
-# set the counter
-X=1
-
-# change directory
-cd /tmp
 
 # make the work directory if it doesn't exist
 if [ ! -d "/tmp/work" ]; then
@@ -12,39 +7,42 @@ fi
 # empty anything old out of the work folder
 rm -r /tmp/work/*
 
-for f in *.jpg; do
+# counter
+X=1
+# process gifs
+for f in /tmp/downloaded-gifs/*.gif; do
 	# make the new name
 	echo "Processing" $f
 	NEWNAME=$(printf %04d.%s ${X%.*} ${f##*.})
 	NEWNAME=${NEWNAME:0:4}
 	let X="$X+1"
 
-	# rotate, scale, convert to png
-	convert $f -gravity center \
+	# rotate, scale
+	convert $f -background white \
+	  -gravity center \
 	  -scale 720 -extent 1280x720 \
-	  -background white PNG24:/tmp/work/$NEWNAME.png
+	  /tmp/prepped-gifs/$NEWNAME.gif
 
-	# add watermark
-	if [ -f "/tmp/watermark.png" ]; then
-		convert /tmp/watermark.png -scale 720 /tmp/watermark.png
-		composite /tmp/watermark.png -geometry +280+0 /tmp/work/$NEWNAME.png /tmp/work/$NEWNAME.png
-	fi
+	printf "file '%s'\n" /tmp/prepped-gifs/$NEWNAME.gif >> list.txt
+
+	# add watermark TODO: add to previous step
+	#if [ -f "/tmp/watermark.png" ]; then
+		#convert /tmp/watermark.png -scale 720 /tmp/watermark.png
+		#composite /tmp/watermark.png -geometry +280+0 /tmp/work/$NEWNAME.png /tmp/work/$NEWNAME.png
+	#fi
 done
 
-# convert to endcard to PNG
+# convert endcard to PNG
 convert /tmp/endcard.jpg PNG24:/tmp/endcard.png
 
-# copy the end card for it to be added to the end
-for i in {1..45}; do
-	NEWNAME=$(printf %04d.%s ${X%.*} ${f##*.})
-	NEWNAME=${NEWNAME:0:4}
-	let X="$X+1"
-	cp /tmp/endcard.png /tmp/work/$NEWNAME.png
-done
+# add endcard to the end of the .gif list
+for i in {1..45}; do printf "file '%s'\n" /tmp/endcard.png >> list.txt; done
 
 # make the mp4
 ffmpeg -r 15 \
-  -i /tmp/work/%04d.png \
+  -f \
+  concat \
+  -i list.txt \
   -i /tmp/song.mp3 \
   -map 0:v \
   -map 1:a \
@@ -54,4 +52,10 @@ ffmpeg -r 15 \
   -strict experimental \
   -b:a 192k \
   -shortest -y /tmp/timelapse.mp4
+
+#ffmpeg -y -f concat -i list.txt -c copy $CODE-final.mp4
+
+
+
+
 

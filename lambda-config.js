fe2cce9a3fd14fdbcc100ded6022d962bd9e524d
Russell Matney <russell.matney@gmail.com>
2015-04-15 16:50:11 -0400

    lambda-config formatting update

--- lambda-config.js
+++ lambda-config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  region: 'us-east-1',
-  handler: 'index.handler',
-  role: 'arn:aws:iam::106586740595:role/executionrole',
-  functionName: 'create-timelapse',
-  timeout: 60
+  FunctionName: 'create-timelapse',
+  Handler: 'index.handler',
+  Role: 'arn:aws:iam::106586740595:role/executionrole',
+  Region: 'us-east-1',
+  Runtime: 'nodejs',
+  Timeout: 60
 }
